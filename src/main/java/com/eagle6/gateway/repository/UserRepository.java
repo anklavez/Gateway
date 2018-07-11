eddd47736047733e420eb43f0376c0d30f98aa12
anklavez@gmail.com <1qa2wsQAZwsx>
2018-07-12 00:03:39 +0400

    Initial commit

---
+++ src/main/java/com/eagle6/gateway/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.eagle6.gateway.repository;
+
+import com.eagle6.gateway.domain.User;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+import java.time.Instant;
+
---
+++ src/main/webapp/app/admin/admin.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { GatewaySharedModule } from 'app/shared';
+/* jhipster-needle-add-admin-module-import - JHipster will add admin modules imports here */
+
+import {
+    adminState,
+    UserMgmtComponent,
+    UserMgmtDetailComponent,
+    UserMgmtUpdateComponent,
+    UserMgmtDeleteDialogComponent,
+    LogsComponent,
+    JhiMetricsMonitoringModalComponent,
+    JhiMetricsMonitoringComponent,
+    JhiHealthModalComponent,
+    JhiHealthCheckComponent,
+    JhiConfigurationComponent,
+    JhiDocsComponent,
+    JhiGatewayComponent
+} from './';
+
+@NgModule({
+    imports: [
+        GatewaySharedModule,
+        RouterModule.forChild(adminState)
+        /* jhipster-needle-add-admin-module - JHipster will add admin modules here */
+    ],
+    declarations: [
+        UserMgmtComponent,
+        UserMgmtDetailComponent,
+        UserMgmtUpdateComponent,
+        UserMgmtDeleteDialogComponent,
+        LogsComponent,
+        JhiConfigurationComponent,
+        JhiHealthCheckComponent,
+        JhiHealthModalComponent,
+        JhiDocsComponent,
+        JhiGatewayComponent,
+        JhiMetricsMonitoringComponent,
+        JhiMetricsMonitoringModalComponent
+    ],
+    entryComponents: [UserMgmtDeleteDialogComponent, JhiHealthModalComponent, JhiMetricsMonitoringModalComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class GatewayAdminModule {}
